53ff2427bbebac196381905fa340edbaf23da2d9
Abhsihek Sharma <abhsiheksharma@Abhsiheks-MacBook-Air.local>
2024-08-09 23:02:39 +0530

    added util code

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@
 // })
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import app from './app.js';
 
 dotenv.config({
     path: './env'
 })
 connectDB()
+.then(()=>{
+    app.on('error', ()=>{
+        console.log('Failed to start app', error);
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log(`Mongo DB connection failed !!!`, error);
+})
 /*
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
