5832f63fa6710c968242e595cdc73fd4ebbe14da
Abhsihek Sharma <abhsiheksharma@Abhsiheks-MacBook-Air.local>
2024-08-10 12:57:05 +0530

    add register user and controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,29 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
 
 const registerUser = asyncHandler(async(req, res) => {
+    // Steps to register a user
+    // 1. Get user details from frontend
+    // 2. Validate the request body
+    // 3. Check if user already exists
+    // 4. Check for images, check for avatar images
+    // 5. upload them to cloudnary and 
+    // 6. Create user object
+    // 7. Remove password and refresh token feild from response
+    // 8. Check for user creation
+    // 9. Save the user to the database
+
+    // Simulating user registration
+    const { email } = req.body;
+    console.log('email', email);
+    // const user = {
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password // Assuming password is hashed
+    // }
+
+    // Save the user to the database
+    // Example: await User.create(user)
+
     res.status(200).json({
         message: 'User registered successfully'
     })
--- src/utils/cloudnary.js
+++ src/utils/cloudnary.js
@@ -1,48 +1,31 @@
 import { v2 as cloudinary } from 'cloudinary';
 import fs from 'fs';
 
-;(async function(localFilePath) {
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+});
 
+const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFilePath) return;
+        if (!localFilePath) return null;
 
         //upload the file on cloudinary server
-        const result = await cloudinary.uploader.upload(localFilePath, {
+        const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: 'auto',
         });
 
         // file has been successfully uploaded
-        console.log('File uploaded successfully:', result.url, result);
-        return result; 
+        console.log('File uploaded successfully:', response.url, response);
+        return response;
 
         // Delete the local file
     } catch (error) {
         fs.unlinkSync(localFilePath);
         // remove the local saved temporary file as upload failed
+        return null;
     }
-
-    // Configuration
-    cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
-    });
-    
-    // Optimize delivery by resizing and applying auto-format and auto-quality
-    const optimizeUrl = cloudinary.url('shoes', {
-        fetch_format: 'auto',
-        quality: 'auto'
-    });
-    
-    console.log(optimizeUrl);
-    
-    // Transform the image: auto-crop to square aspect_ratio
-    const autoCropUrl = cloudinary.url('shoes', {
-        crop: 'auto',
-        gravity: 'auto',
-        width: 500,
-        height: 500,
-    });
-    
-    console.log(autoCropUrl);    
-})();
+}
+export { uploadOnCloudinary }
